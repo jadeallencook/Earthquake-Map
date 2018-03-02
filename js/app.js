ecd0ee584a9a117d4d47eb4ec1bdf2f59f64ecf7
Jade Allen Cook <jadeallencook@MacBook-Pro-3.local>
2018-03-02 08:30:19 -0700

    🕹 Added interactivity to map

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
     var map = L.map('map').setView([40.7607793, -111.89104739999999], 13);
 
     // create base map
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Developed by <a href="#" target="_blank">Jade Allen Cook</a> & <a href="#" target="_blank">Geoffery Thatcher</a> for <a href="#" target="_blank">Deseret News</a>',
         maxZoom: 18,
         id: 'mapbox.streets',
@@ -17,7 +17,7 @@
 
     // determine correct color
     function getColor(percent) {
-        percent = Math.round(percent * 100);
+        if (percent < 1) percent = Math.round(percent * 100);
         return percent < 20 ? '#fdf5f1' :
             percent < 40 ? '#f3c0a9' :
             percent < 60 ? '#e9785a' :
@@ -81,5 +81,54 @@
         style: style,
         onEachFeature: onEachFeature
     }).addTo(map);
+
+    var info = L.control();
+
+    info.onAdd = function (map) {
+        this._div = L.DomUtil.create('div', 'info');
+        this.update();
+        return this._div;
+    };
+
+    // method that we will use to update the control based on feature properties passed
+    info.update = function (props) {
+        this._div.innerHTML = '<h4>Will It Topple?</h4>';
+        if (props !== undefined) {
+            this._div.innerHTML += 'There\'s a <b>' + props.percent + '%</b> of fall<br />';
+            this._div.innerHTML += 'Average build is <b>' + props.year + '</b>';
+        } else {
+            this._div.innerHTML += 'Hover over a section';
+        }
+    };
+
+    info.addTo(map);
+
+    function highlightFeature(e) {
+        info.update({
+            year: e.target.feature.properties.AvgYearBul,
+            percent: Math.round(e.target.feature.properties.URM_URM_Pe * 100)
+        });
+    }
+
+    function resetHighlight(e) {
+        info.update();
+    }
+
+    var legend = L.control({
+        position: 'bottomright'
+    });
+
+    legend.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [20, 40, 60, 80, 100],
+            labels = [];
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML += '<i style="background:' + getColor(grades[i] - 1) + '"></i> ';
+            div.innerHTML += (grades[i] - 20) + '&ndash;' + grades[i] + '%<br />';
+        }
+        return div;
+    };
+
+    legend.addTo(map);
 
 })();
